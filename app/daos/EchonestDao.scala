5c48e543c673b0c183f1a76c59df5502888d0508
Chirag Khatri <ckhatri4@gmail.com>
2013-12-29 22:03:50 -0800

    Figured out that Echnoest taste profiles won't be a good use case for me. So now I'm working on caching their data. Still haven't determined what sort of data store to use

--- app/daos/EchonestDao.scala
+++ app/daos/EchonestDao.scala
@@ -1,13 +1,15 @@
 package daos
 
+import java.net.URLEncoder
+
 import scala.Option.option2Iterable
 import scala.io.Source
+import scala.util.Failure
+import scala.util.Success
 
 import helpers.Common.cfg
-import play.api.libs.json.JsArray
-import play.api.libs.json.JsString
-import play.api.libs.json.JsValue
-import play.api.libs.json.Json
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json.{ JsArray, JsString, JsValue, Json }
 import play.api.libs.ws.WS
 
 /**
@@ -21,6 +23,13 @@ object EchonestDao {
   def main(args: Array[String]) {
     val rawJson = Source.fromFile(args(0)).mkString
     val trackTuples = getTrackInfoFromJson(rawJson)
+    for(trackTuple <- trackTuples) {
+      val futureResponse = getEchonestDataForTrack(trackTuple._1, trackTuple._2)
+      futureResponse.onComplete {
+        case Success(json) => println(json.toString)
+        case Failure(err) =>  println(err)
+      }
+    }
   }
 
   // Returns a sequence of 2-tuples - (artist, track)
@@ -62,13 +71,62 @@ object EchonestDao {
     val title       = s"&title$trackName"
     val buckets     = "&bucket=id:rdio-US&bucket=audio_summary&bucket=tracks"
 
-    return baseUrl + apiKey + format + resultLimit + artist + title + buckets
+    return URLEncoder.encode(baseUrl + apiKey + format + resultLimit + artist + title + buckets, "UTF-8")
   }
   
   def getEchonestDataForTrack(artistName: String, trackName: String) = {
     val url = formatEchonestUrl(artistName, trackName)
-    WS.url(url).get()
-    // TODO
+    val futureResponse = WS.url(url).get()
+    val futureResult = futureResponse.map { response =>
+      // We are currently capped at 20 Echonest calls per minute. On average
+      // each call takes less than 200ms. That means To complete 20 requests
+      // we'll only spend 4 seconds. So for now we must wait the remaining 56
+      // seconds to continue making Echonest calls :(
+      val apiCallLimit = response.header("X-RateLimit-Remaining").get.toInt
+      if(apiCallLimit <= 0) { Thread.sleep(56000) }
+      response.json
+    }
+    futureResult
   }
+  
+  case class Track(id: String, artistName: String, trackName: String, summary: String)
+  def storeEchonestDataForTrack(json: JsValue) {
 
+    def getStatusCodeMsg(json : JsValue) : (String, String) = {
+      val code = json \ "code"
+      val msg  = json \ "message"
+      (code.toString, msg.toString)
+    }
+    
+    def deserializeTrack(json: JsValue) = {
+      json match {
+        case JsArray(arr) => {
+          val trackJson = arr(0)
+          val id          = trackJson \ "id"
+          val artistName  = trackJson \ "artist_name"
+          val trackName   = trackJson \ "title"
+          val summary     = trackJson \ "audio_summary"
+          val track = Track(id.toString, artistName.toString, trackName.toString, summary.toString)
+          Some(track)
+        }
+        case _ => None
+      }
+    }
+    
+    def storeTrack(track: Track) {
+      
+    }
+
+    val status = json \ "response" \ "status"
+    val audioSummary = getStatusCodeMsg(status)  match {
+      case ("0", msg) => {
+        deserializeTrack(json \ "response" \ "songs") match {
+          case Some(t) => storeTrack(t)
+          case None    => println("Retrieved unexpected Json format")
+        }
+      }
+      case (_, msg)   => println(s"Could not retreive track data. Msg: $msg")
+    }
+  }
+    
 }
