f3331debc76bff4908553f8316bf15784670b0f2
Chirag Khatri <ckhatri4@gmail.com>
2014-01-12 17:31:43 -0800

    Got storage script mostly working. Has some quirks I need to work out

--- build.sbt
+++ build.sbt
@@ -2,6 +2,11 @@ name := "aura"
 
 version := "1.0-SNAPSHOT"
 
+resolvers ++= Seq(
+  "Typesafe Releases" at "http://typesafe.artifactoryonline.com/typesafe",
+  "pk11 repo" at "http://pk11-scratch.googlecode.com/svn/trunk"
+)
+
 libraryDependencies ++= Seq(
   jdbc,
   anorm,
--- script/EchonestDao.scala
+++ script/EchonestDao.scala
@@ -1,5 +1,3 @@
-package daos
-
 import java.net.URLEncoder
 
 import scala.Option.option2Iterable
@@ -7,10 +5,18 @@ import scala.io.Source
 import scala.util.Failure
 import scala.util.Success
 
+import com.redis.RedisClient
+
 import helpers.Common.cfg
+import play.api.Logger
+import play.api.Logger._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.json.{ JsArray, JsString, JsValue, Json }
+import play.api.libs.json.JsArray
+import play.api.libs.json.JsString
+import play.api.libs.json.JsValue
+import play.api.libs.json.Json
 import play.api.libs.ws.WS
+import play.core.StaticApplication
 
 /**
  * This is meant to be ran as a stand alone script. This script consumes a Json
@@ -21,19 +27,14 @@ import play.api.libs.ws.WS
 object EchonestDao {
 
   def main(args: Array[String]) {
+    val app = new StaticApplication(new java.io.File("."))
     val rawJson = Source.fromFile(args(0)).mkString
     val trackTuples = getTrackInfoFromJson(rawJson)
-    for(trackTuple <- trackTuples) {
-      val futureResponse = getEchonestDataForTrack(trackTuple._1, trackTuple._2)
-      futureResponse.onComplete {
-        case Success(json) => println(json.toString)
-        case Failure(err) =>  println(err)
-      }
-    }
+    storeEchonestDataForTracks(trackTuples, 20)
   }
 
   // Returns a sequence of 2-tuples - (artist, track)
-  def getTrackInfoFromJson(rawJson: String): Seq[(String, String)] = {
+  def getTrackInfoFromJson(rawJson: String): List[(String, String)] = {
 
     def extractJsString(jsVal: JsValue) = {
       jsVal match {
@@ -59,33 +60,30 @@ object EchonestDao {
       }
     }
 
-    trackTuples
+    trackTuples.toList
   }
   
   def formatEchonestUrl(artistName: String, trackName: String) : String = {
+    val encodedArtistName = URLEncoder.encode(artistName, "UTF-8")
+    val encodedTrackName  = URLEncoder.encode(trackName, "UTF-8")
+
     val baseUrl     = "http://developer.echonest.com/api/v4/song/search?"
     val apiKey      = s"api_key=${cfg("echonest.api_key")}"
     val format      = "&format=json"
-    val resultLimit = "&result=1"
-    val artist      = s"&artist=$artistName"
-    val title       = s"&title$trackName"
+    val resultLimit = "&results=1"
+    val artist      = s"&artist=$encodedArtistName"
+    val title       = s"&title=$encodedTrackName"
     val buckets     = "&bucket=id:rdio-US&bucket=audio_summary&bucket=tracks"
 
-    return URLEncoder.encode(baseUrl + apiKey + format + resultLimit + artist + title + buckets, "UTF-8")
+    val url = baseUrl + apiKey + format + resultLimit + artist + title + buckets
+    Logger.info(s"Formatted url for artist - $artistName, track - $trackName: " + url)
+    return url
   }
   
   def getEchonestDataForTrack(artistName: String, trackName: String) = {
     val url = formatEchonestUrl(artistName, trackName)
     val futureResponse = WS.url(url).get()
-    val futureResult = futureResponse.map { response =>
-      // We are currently capped at 20 Echonest calls per minute. On average
-      // each call takes less than 200ms. That means To complete 20 requests
-      // we'll only spend 4 seconds. So for now we must wait the remaining 56
-      // seconds to continue making Echonest calls :(
-      val apiCallLimit = response.header("X-RateLimit-Remaining").get.toInt
-      if(apiCallLimit <= 0) { Thread.sleep(56000) }
-      response.json
-    }
+    val futureResult = futureResponse.map { response => response.json }
     futureResult
   }
   
@@ -106,7 +104,7 @@ object EchonestDao {
           val artistName  = trackJson \ "artist_name"
           val trackName   = trackJson \ "title"
           val summary     = trackJson \ "audio_summary"
-          val track = Track(id.toString, artistName.toString, trackName.toString, summary.toString)
+          val track = Track(id.as[JsString].toString, artistName.toString, trackName.toString, summary.toString)
           Some(track)
         }
         case _ => None
@@ -114,7 +112,13 @@ object EchonestDao {
     }
     
     def storeTrack(track: Track) {
+      val redisClient = new RedisClient("localhost", 6379)
+      val trackMap = Map("artist" -> track.artistName, "track" -> track.trackName, "summary" -> track.summary)
+      val success = redisClient.hmset(track.id, trackMap)
       
+      if (!success) {
+        Logger.error("Error occured while storing track summary")
+      }
     }
 
     val status = json \ "response" \ "status"
@@ -122,11 +126,33 @@ object EchonestDao {
       case ("0", msg) => {
         deserializeTrack(json \ "response" \ "songs") match {
           case Some(t) => storeTrack(t)
-          case None    => println("Retrieved unexpected Json format")
+          case None    => Logger.error("Retrieved unexpected Json format")
         }
       }
-      case (_, msg)   => println(s"Could not retreive track data. Msg: $msg")
+      case (_, msg)   => Logger.error(s"Could not retreive track data. Msg: $msg")
     }
   }
-    
+  
+  def storeEchonestDataForTracks(trackTuples: Seq[(String, String)], limit: Integer) {
+    trackTuples match {
+      case Nil => Logger.info("Finished storing Echonest audio summaries")
+      case first::rest => {
+        if (limit <= 0) {
+	      // We are currently capped at 20 Echonest calls per minute. On average
+	      // each call takes less than 200ms. That means To complete 20 requests
+	      // we'll only spend 4 seconds. So for now we must wait the remaining 56
+	      // seconds to continue making Echonest calls :(
+          Thread.sleep(58000)
+          storeEchonestDataForTracks(trackTuples, 20)
+        } else {
+          val futureResponse = getEchonestDataForTrack(first._1, first._2)
+          futureResponse.onComplete {
+            case Success(json) => storeEchonestDataForTrack(json)
+            case Failure(err) =>  Logger.error(err.toString)
+          }
+          storeEchonestDataForTracks(rest, limit - 1)
+        }
+      }
+    }
+  }
 }
